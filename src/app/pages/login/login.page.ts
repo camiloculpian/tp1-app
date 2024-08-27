e42ad08242f267f25294e0c28209a3f27dd6c14f
camiloculpian <camiloculpian@gmail.com>
2024-08-26 23:25:46 -0300

    modified:   src/app/app.routes.ts 	modified:   src/app/pages/login/login.page.html 	modified:   src/app/pages/login/login.page.scss 	modified:   src/app/pages/login/login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormBuilder, FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonText, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonIcon, IonText, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginPage implements OnInit {
+  formGroup! :FormGroup ;
+  credentials!: FormGroup;
+  showPassword = false;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['test@domain.com', [Validators.required, Validators.email]],
+      password: ['test', [Validators.required, Validators.minLength(4)]],
+    });
+  }
+
+  login() {}
+
+  get email() {
+    return this.credentials.get('email');
+  }
+
+  get password() {
+    return this.credentials.get('password');
+  }
+
+  navigateToRegister() {
+    console.log('navigateToRegister()');
+    this.router.navigate(['register']);
   }
 
 }
