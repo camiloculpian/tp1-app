e5fd0620a50f0c6547de3b6bf55c55da9df71ac4
camiloculpian <camiloculpian@gmail.com>
2024-08-28 19:06:14 -0300

    new file:   android/app/capacitor.build.gradle 	new file:   android/capacitor.settings.gradle 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/app-routing.module.ts 	new file:   src/app/app.module.ts 	modified:   src/app/pages/login/login.page.html 	modified:   src/app/pages/login/login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonText, IonIcon, IonButton } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import {addIcons} from "ionicons";
+import { eyeOutline, eyeOffOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-login',
@@ -13,30 +15,34 @@ import { Router } from '@angular/router';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginPage implements OnInit {
-  formGroup! :FormGroup ;
-  credentials!: FormGroup;
+  public loginForm: FormGroup;
   showPassword = false;
 
   constructor(
-    private fb: FormBuilder,
+    public formBuilder: FormBuilder,
     private router: Router,
-  ) { }
+  ) { 
+      addIcons({ eyeOutline, eyeOffOutline, });
 
-  ngOnInit() {
-    this.credentials = this.fb.group({
-      email: ['test@domain.com', [Validators.required, Validators.email]],
-      password: ['test', [Validators.required, Validators.minLength(4)]],
-    });
+      this.loginForm  = formBuilder.group({
+        email: ['test@dominio.com', [Validators.required, Validators.email]],
+        password: ['test', [Validators.required, Validators.minLength(4)]],
+      });
   }
 
-  login() {}
+  ngOnInit() {
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 
   get email() {
-    return this.credentials.get('email');
+    return this.loginForm.get('email');
   }
 
   get password() {
-    return this.credentials.get('password');
+    return this.loginForm.get('password');
   }
 
   navigateToRegister() {
