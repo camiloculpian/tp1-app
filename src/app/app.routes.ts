3f63ff0c2b3c42ffa2eef56f4b4a24c09d72efde
camiloculpian <camiloculpian@gmail.com>
2024-08-28 20:01:36 -0300

    modified:   src/app/app.routes.ts 	deleted:    src/app/guards/profile.guard.spec.ts 	deleted:    src/app/guards/profile.guard.ts 	modified:   src/app/pages/home/home.page.html 	modified:   src/app/pages/home/home.page.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { loginGuard } from './guards/login.guard';
-import { profileGuard } from './guards/profile.guard';
 
 export const routes: Routes = [
   {
@@ -26,6 +25,6 @@ export const routes: Routes = [
   {
     path: 'profile',
     loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage),
-    canActivate: [profileGuard],
+    canActivate: [authGuard],
   },
 ];
