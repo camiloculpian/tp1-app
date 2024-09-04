10458d88d64897e44fbef4b2cde4eeac68158ebc
camiloculpian <camiloculpian@gmail.com>
2024-09-04 20:20:26 -0300

    modified:   src/app/layouts/main-content/main-content.component.html 	modified:   src/app/pages/profile/profile.page.html 	modified:   src/app/pages/register/register.page.html 	modified:   src/app/pages/register/register.page.scss 	modified:   src/app/pages/register/register.page.ts

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonText, IonInput, IonLabel, IonItem, IonAvatar, IonCol } from '@ionic/angular/standalone';
+import { addIcons } from "ionicons";
+import { person } from "ionicons/icons";
+import { environment } from 'src/app/app.component';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCol, IonAvatar, IonItem, IonLabel, IonInput, IonText, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class RegisterPage implements OnInit {
+  public userDataForm!: FormGroup;
+  constructor(public formBuilder: FormBuilder,) {
+      addIcons({person}); }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+      ngOnInit() {
+        this.userDataForm  = this.formBuilder.group({
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(4)]],
+          lastName: ['', [Validators.required, Validators.email]],
+          name: ['', [Validators.required, Validators.email]],
+        });
+      }
 
 }
